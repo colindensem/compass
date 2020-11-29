7bd58b95581e4f0a5191aeba75cad8cba557b2b7
Colin Densem <colin@summit360.co.uk>
2020-11-29 12:47:41 +0000

    ci: prepare for github ci && mix test

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,10 @@ use Mix.Config
 
 # Configure your database
 config :compass, Compass.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("PGUSER", "postgres"),
+  password: System.get_env("PGPASSWORD", "postgres"),
   database: "compass_dev",
-  hostname: "localhost",
+  hostname: System.get_env("PGHOST", "localhost"),
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
