90fcbf68f6c85315e1637c2ef232fee88b5b809b
Colin Densem <colin@summit360.co.uk>
2020-12-03 18:18:06 +0000

    deploy: force ssl in production

--- config/prod.exs
+++ config/prod.exs
@@ -77,7 +77,8 @@ config :compass, CompassWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
-  server: true
+  server: true,
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 config :compass, Compass.Repo,
   adapter: Ecto.Adapters.Postgres,
